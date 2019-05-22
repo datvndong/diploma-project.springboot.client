fa4146c7632e249e5fb2e45fc2759b88aabebba4
Dat Nguyen <vandatnguyen1896@gmail.com>
2019-05-23 01:41:18 +0700

    update form progress bar v1

--- src/main/java/springboot/centralizedsystem/controllers/FormController.java
+++ src/main/java/springboot/centralizedsystem/controllers/FormController.java
@@ -1,6 +1,9 @@
 package springboot.centralizedsystem.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -10,12 +13,13 @@ import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.UnknownHttpStatusCodeException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import springboot.centralizedsystem.domains.Form;
@@ -42,36 +46,82 @@ public class FormController {
             ResponseEntity<String> res = new RestTemplate().exchange(APIs.LIST_FORMS_URL, HttpMethod.GET, entity,
                     String.class);
 
-            if (res.getStatusCode() == HttpStatus.OK) {
-                JSONArray jsonArray = new JSONArray(res.getBody());
-                JSONObject jsonObject = null;
-                List<Form> list = new ArrayList<>();
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    jsonObject = (JSONObject) jsonArray.get(i);
-                    if (jsonObject.get("type").equals("form")) {
-                        String name = jsonObject.getString("name");
-                        String title = jsonObject.getString("title");
-                        String path = jsonObject.getString("path");
-                        int amount = getAmount(entity, path);
-                        list.add(new Form(name, title, path, amount, "", "May 15, 2015", "", ""));
-                    }
-                }
-                model.addAttribute("title", "Forms management");
-                model.addAttribute("list", list);
-            }
+            JSONArray jsonArray = new JSONArray(res.getBody());
+            JSONObject jsonObject = null;
+            List<Form> list = new ArrayList<>();
+//            for (int i = 0; i < jsonArray.length(); i++) {
+//                jsonObject = (JSONObject) jsonArray.get(i);
+//                if (jsonObject.get("type").equals("form")) {
+//                    String name = jsonObject.getString("name");
+//                    String title = jsonObject.getString("title");
+//                    String path = jsonObject.getString("path");
+//                    int amount = getAmount(entity, path);
+//                    list.add(new Form(name, title, path, amount, "", ""));
+//                }
+//            }
+            list.add(
+                    new Form("a", "a", "a", 1, null, null,
+                            getDurationPercent("05/18/2019 00:01:00", "05/30/2019 05:15:00"),
+                            getTypeProgressBar(getDurationPercent("05/18/2019 00:01:00", "05/30/2019 05:15:00"))));
+//            list.add(
+//                    new Form("b", "b", "b", 2, null, null, getDurationPercent("05/22/2019 01:01", "05/26/2019 05:15"),
+//                            getTypeProgressBar(getDurationPercent("05/22/2019 01:01", "05/26/2019 05:15"))));
+//            list.add(
+//                    new Form("c", "c", "c", 3, null, null, getDurationPercent("05/21/2019 00:01", "05/24/2019 05:15"),
+//                            getTypeProgressBar(getDurationPercent("05/21/2019 00:01", "05/24/2019 05:15"))));
+//            list.add(
+//                    new Form("d", "d", "d", 4, null, null, getDurationPercent("05/19/2019 00:01", "05/26/2019 05:15"),
+//                            getTypeProgressBar(getDurationPercent("05/19/2019 14:01", "05/26/2019 05:15"))));
+            list.add(new Form("e", "e", "e", 5, null, null,
+                    getDurationPercent("05/18/2019 00:01:00", "05/24/2019 05:15:00"),
+                    getTypeProgressBar(getDurationPercent("05/18/2019 00:01:00", "05/24/2019 05:15:00"))));
+
+            model.addAttribute("list", list);
+
+            model.addAttribute("title", "Forms management");
             return Views.FORMS;
-        } catch (NullPointerException e) {
+        } catch (NullPointerException | HttpClientErrorException | UnknownHttpStatusCodeException e) {
             redirect.addFlashAttribute(Errors.LOGIN, Messages.TOKEN_EXPIRED_MESSAGE);
             return "redirect:" + RequestsPath.LOGIN;
+        } catch (ParseException e) {
+            return Views.ERROR_404;
         }
     }
 
-    private int getAmount(HttpEntity<String> entity, String path) throws NullPointerException {
+    private int getAmount(HttpEntity<String> entity, String path)
+            throws HttpClientErrorException, UnknownHttpStatusCodeException {
         ResponseEntity<String> res = new RestTemplate().exchange(APIs.getListSubmissionsURL(path), HttpMethod.GET,
                 entity, String.class);
-        if (res.getStatusCode() == HttpStatus.OK) {
-            return new JSONArray(res.getBody()).length();
+        return new JSONArray(res.getBody()).length();
+    }
+
+    private int getDurationPercent(String start, String end) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        Date dateStart = format.parse(start);
+        Date dateEnd = format.parse(end);
+
+        // in milliseconds
+        long diffStartEnd = dateEnd.getTime() - dateStart.getTime();
+        long diffStartNow = new Date().getTime() - dateStart.getTime();
+
+        int result = (int) Math.round((double) diffStartNow / diffStartEnd * 100);
+        if (result > 100) {
+            result = 100;
         }
-        return -1;
+
+        return result;
+    }
+
+    private String getTypeProgressBar(int percent) {
+        if (percent < 25) {
+            return "success";
+        } else if (percent < 50) {
+            return "primary";
+        } else if (percent < 75) {
+            return "warning";
+        } else if (percent < 100) {
+            return "danger";
+        }
+        return "info";
     }
 }
--- src/main/java/springboot/centralizedsystem/domains/Form.java
+++ src/main/java/springboot/centralizedsystem/domains/Form.java
@@ -6,10 +6,10 @@ public class Form {
     private String title;
     private String path;
     private int amount;
-    private String startDate;
-    private String endDate;
-    private String startTime;
-    private String endTime;
+    private String start;
+    private String end;
+    private int durationPercent;
+    private String typeProgressBar;
 
     public String getName() {
         return name;
@@ -35,38 +35,6 @@ public class Form {
         this.amount = amount;
     }
 
-    public String getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
-
-    public String getStartTime() {
-        return startTime;
-    }
-
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
-
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
     public String getPath() {
         return path;
     }
@@ -75,19 +43,51 @@ public class Form {
         this.path = path;
     }
 
-    public Form(String name, String title, String path, int amount, String startDate, String endDate, String startTime,
-            String endTime) {
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    public int getDurationPercent() {
+        return durationPercent;
+    }
+
+    public void setDurationPercent(int durationPercent) {
+        this.durationPercent = durationPercent;
+    }
+
+    public String getTypeProgressBar() {
+        return typeProgressBar;
+    }
+    
+    public void setTypeProgressBar(String typeProgressBar) {
+        this.typeProgressBar = typeProgressBar;
+    }
+    
+    public Form(String name, String title, String path, int amount, String start, String end, int durationPercent,
+            String typeProgressBar) {
         super();
         this.name = name;
         this.title = title;
         this.path = path;
         this.amount = amount;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.startTime = startTime;
-        this.endTime = endTime;
+        this.start = start;
+        this.end = end;
+        this.durationPercent = durationPercent;
+        this.typeProgressBar = typeProgressBar;
     }
-    
+
     public Form() {
         super();
     }
